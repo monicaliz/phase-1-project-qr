4167bc7e03064b01e1826c475ef1d24e9b03d539
monicaliz <monicaeliz3@gmail.com>
2022-08-10 21:29:42 -0400

    JS: added submit button event listener

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //color
-const mainColorPicker = document.querySelector('#color');
-const qrColorPicker = document.querySelector('#color-value')
+const qrColorPicker = document.querySelector('#color');
+const qrColorValue = document.querySelector('#color-value')
 
 const bgColorPicker = document.querySelector('#bg-color')
 const bgColorValue = document.querySelector("#color-bg-value")
 
 const updateColor = e => {
     const value = e.target.value;
-    mainColorValue.innerText = value;
+    qrColorValue.innerText = value;
 }
 
 const updateBgColor = e => {
@@ -16,28 +16,28 @@ const updateBgColor = e => {
 }
 
 const addColorPickerEventListeners = () => {
-    mainColorPicker.addEventListener('change', updateColor)
+    qrColorPicker.addEventListener('change', updateColor);
     bgColorPicker.addEventListener('change', updateBgColor);
-}
+};
 
 addColorPickerEventListeners();
 
 //sliders
 const sizeSlider = document.querySelector('#size');
-const marginValue = document.querySelector('#size-value');
+const sizeValue = document.querySelector('#size-value');
 
 const marginSlider = document.querySelector('#margin')
 const marginValue = document.querySelector('#margin-value')
 
-const updateSize = e = => {
+const updateSize = e => {
     const value = e.target.value;
-    sizeValue.innerText = `${value} x ${value}` 
-}
+    sizeValue.innerText = `${value} x ${value}`;
+};
 
-const updateMargin = e = => {
+const updateMargin = e => {
     const value = e.target.value;
-    marginValue.innerText = `${value} px` 
-}
+    marginValue.innerText = `${value} px`;
+};
 
 const addSliderEventListeners = () => {
     sizeSlider.addEventListener('change', updateSize);
@@ -45,3 +45,15 @@ const addSliderEventListeners = () => {
 }
 
 addSliderEventListeners();
+
+const submitButton = document.querySelector('#submit');
+
+const onSubmit = () => {
+    console.log("clicked")
+}
+
+const addSubmitEventListener = () => {
+    submitButton.addEventListener('click', onSubmit)
+}
+
+addSubmitEventListener();
